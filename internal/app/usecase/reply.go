3175b4c68cf7ab5f7606dd9dad004a16c13c44fa
NAGATA Hiroaki <nagata@handlena.me>
2025-04-19 00:57:20 +0900

    feat: reply to mention

---
+++ internal/app/usecase/reply.go
@@ -0,0 +1,58 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/handlename/otomo/internal/domain/entity"
+	"github.com/handlename/otomo/internal/domain/event"
+	"github.com/handlename/otomo/internal/infra/service"
+	"github.com/morikuni/failure/v2"
+	"github.com/rs/zerolog/log"
+)
+
+type ReplyInput struct {
+	EventData event.InstructionReceivedData
+}
+
+type ReplyOutput struct{}
+
+type Reply struct {
+	otomo *entity.Otomo
+	slack *service.Slack // TODO: repalce with service.Messenger
+}
+
+func NewReply(otomo *entity.Otomo, slack *service.Slack) *Reply {
+	return &Reply{
+		otomo: otomo,
+		slack: slack,
+	}
+}
+
+func (r *Reply) Run(ctx context.Context, input ReplyInput) (*ReplyOutput, error) {
+	rep, err := r.otomo.Think(ctx,
+		*entity.NewContext(""),
+		entity.NewInstruction("", input.EventData.RawInstruction),
+	)
+	if err != nil {
+		return nil, failure.Wrap(err)
+	}
+
+	err = r.slack.PostMessage(ctx, input.EventData.ChannelID, input.EventData.MessageID, rep.Body())
+	return &ReplyOutput{}, err
+}
+
+func (r *Reply) Subscribe(publisher event.Publisher) {
+	publisher.Subscribe(event.KindInstructionReceived, func(ctx context.Context, eev event.Event) error {
+		ev, ok := eev.(*event.InstructionReceived)
+		if !ok {
+			log.Error().Msg("failed to assert event")
+			return nil
+		}
+
+		input := ReplyInput{
+			EventData: ev.Data().(event.InstructionReceivedData),
+		}
+		_, err := r.Run(ctx, input)
+		return err
+	})
+}
--- internal/infra/ui/http/slack/slack.go
+++ internal/infra/ui/http/slack/slack.go
@@ -6,15 +6,22 @@ import (
 
 	"github.com/handlename/otomo/config"
 	"github.com/handlename/otomo/internal/app/usecase"
+	"github.com/handlename/otomo/internal/domain/entity"
+	"github.com/handlename/otomo/internal/infra/repository"
 	"github.com/handlename/otomo/internal/infra/service"
 	"github.com/handlename/otomo/internal/infra/ui/http/middleware"
+	"github.com/samber/lo"
 )
 
 func New(ctx context.Context, prefix string) http.Handler {
 	slack := service.NewSlack(config.Config.Slack.BotToken, config.Config.Slack.SigningSecret)
+	repoBrain := repository.NewGeneralBrain(ctx)
+	brain := lo.Must(repoBrain.New(ctx))
+	otomo := entity.NewOtomo(brain)
 
 	publisher := service.NewEventPublisher()
 	usecase.NewAckInstruction(slack).Subscribe(publisher)
+	usecase.NewReply(otomo, slack).Subscribe(publisher)
 
 	reg := NewRegistry(ctx, publisher, slack)
 	mids := []middleware.Middleware{
