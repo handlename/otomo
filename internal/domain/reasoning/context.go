06e1ff4ca118ade9cbae5dfec5918c6358153239
NAGATA Hiroaki <nagata@handlena.me>
2026-06-15 22:28:57 +0900

    domain: refactor core.Message and update reasoning.Context messages to pointer slice

--- internal/domain/reasoning/context.go
+++ internal/domain/reasoning/context.go
@@ -11,14 +11,16 @@ import (
 type Context struct {
 	systemPrompt *core.Prompt
 	userPrompt   *core.Prompt
-	messages     []core.Message
+	messages     []*core.Message
 }
 
 func NewContext() *Context {
+	systemPrompt, _ := core.NewPrompt("", "", []*core.Prompt{})
+	userPrompt, _ := core.NewPrompt("", "", []*core.Prompt{})
 	return &Context{
-		systemPrompt: core.NewPrompt("", "", []*core.Prompt{}),
-		userPrompt:   core.NewPrompt("", "", []*core.Prompt{}),
-		messages:     []core.Message{},
+		systemPrompt: systemPrompt,
+		userPrompt:   userPrompt,
+		messages:     []*core.Message{},
 	}
 }
 
@@ -27,33 +29,35 @@ func (c *Context) GetUserPrompt() *core.Prompt {
 }
 
 func (c *Context) SetSystemPrompt(body string) {
-	c.systemPrompt = core.NewPrompt(core.PromptTagSystem, body, []*core.Prompt{})
+	c.systemPrompt, _ = core.NewPrompt(core.PromptTagSystem, body, []*core.Prompt{})
 }
 
 func (c *Context) SetUserPrompt(body string) {
-	c.userPrompt = core.NewPrompt(core.PromptTagUser, body, []*core.Prompt{})
+	c.userPrompt, _ = core.NewPrompt(core.PromptTagUser, body, []*core.Prompt{})
 }
 
-func (c *Context) SetMessages(messages []core.Message) {
+func (c *Context) SetMessages(messages []*core.Message) {
 	c.messages = messages
 }
 
 func (c *Context) Prompt() *core.Prompt {
-	return core.NewPrompt(
+	prompt, _ := core.NewPrompt(
 		"",
 		"",
 		[]*core.Prompt{
 			c.systemPrompt,
-			core.NewPrompt("thread", "", lo.Map(c.messages, func(msg core.Message, _ int) *core.Prompt {
+			lo.Must(core.NewPrompt("thread", "", lo.Map(c.messages, func(msg *core.Message, _ int) *core.Prompt {
 				var tag core.PromptTag
-				if msg.User != "" {
-					tag = core.PromptTag(fmt.Sprintf("message user=%s", msg.User))
+				if msg.User() != "" {
+					tag = core.PromptTag(fmt.Sprintf("message user=%s", msg.User()))
 				} else {
-					tag = core.PromptTag(fmt.Sprintf("message role=%s", msg.Role))
+					tag = core.PromptTag(fmt.Sprintf("message role=%s", msg.Role()))
 				}
-				return core.NewPrompt(tag, msg.Body, nil)
-			})),
+				p, _ := core.NewPrompt(tag, msg.Body(), nil)
+				return p
+			}))),
 			c.userPrompt,
 		},
 	)
+	return prompt
 }
