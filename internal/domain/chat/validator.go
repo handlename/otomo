bb999a2dcf7c8bc3b49f75f656661c176b16362e
NAGATA Hiroaki <nagata@handlena.me>
2026-06-15 22:29:23 +0900

    domain: refactor InstructionReceivedData to encapsulate fields and use inline validation

--- internal/domain/chat/validator.go
+++
@@ -1,9 +0,0 @@
-package chat
-
-import "github.com/go-playground/validator/v10"
-
-var validate *validator.Validate
-
-func init() {
-	validate = validator.New(validator.WithRequiredStructEnabled())
-}
